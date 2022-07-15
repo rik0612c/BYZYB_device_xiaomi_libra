9f68b1de9134906aceb321d2da175cb8a7e3275d
Ali Hasan <ahb7671@gmail.com>
2022-07-15 11:45:26 +0530

    libra: Graphics: Stop spamming logcat
    I/OpenGLRenderer(24600): Davey! duration=9223369322898ms; Flags=0, FrameTimelineVsyncId=340569, IntendedVsync=2713955793531, Vsync=2713955793531, InputEventId=0, HandleInputStart=2713956632401, AnimationStart=2713956635421, PerformTraversalsStart=2713956867974, DrawStart=2713990935057, FrameDeadline=2713979793530, FrameInterval=2713956627296, FrameStartTime=8333333, SyncQueued=2714006196671, SyncStart=2714006305474, IssueDrawCommandsStart=2714011599796, SwapBuffers=2714022889484, FrameCompleted=9223372036854775807, DequeueBufferDuration=59532, QueueBufferDuration=1157500, GpuCompleted=9223372036854775807, SwapBuffersCompleted=2714024724276, DisplayPresentTime=0,
    
    Signed-off-by: Ali Hasan <ahb7671@gmail.com>

--- system_prop.mk
+++ system_prop.mk
@@ -89,7 +89,8 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.surface_flinger.force_hwc_copy_for_virtual_displays=true \
     ro.surface_flinger.max_frame_buffer_acquired_buffers=3 \
     ro.surface_flinger.max_virtual_display_dimension=2048 \
-    ro.surface_flinger.protected_contents=true
+    ro.surface_flinger.protected_contents=true \
+    persist.log.tag.OpenGLRenderer=S
 
 # I/O scheduler
 PRODUCT_PROPERTY_OVERRIDES += \
