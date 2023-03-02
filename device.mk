5e1a09aeefd1ae0838d18d2c8df547e08fcada70
rik0612c <qq872638399@gmail.com>
2023-03-02 15:24:45 +0800

    libra: add xiaomi IR HAL from LineageOS

--- device.mk
+++ device.mk
@@ -304,3 +304,10 @@ PRODUCT_PACKAGES += \
     readmac \
     wpa_supplicant \
     wpa_supplicant.conf
+    
+# Consumer IR
+PRODUCT_PACKAGES += \
+    android.hardware.ir@1.0-service.xiaomi
+
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.consumerir.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.consumerir.xml
